f97ff41a738f572a51555afd9ecd2855d0bdc9e3
Nathan Racklyeft <loudnate@gmail.com>
2015-06-14 09:19:48 +0200

    Documentation updates

--- openapscontrib/mmhistorytools/__init__.py
+++ openapscontrib/mmhistorytools/__init__.py
@@ -41,21 +41,37 @@ def get_uses(device, config):
 
 
 class cleanup(Use):
-    """Modifies Medtronic read_history_data output to account for inconsistencies.
+    """Removes inconsistencies from a sequence of pump history
 
-    Specifically:
-     - De-duplicates BolusWizard records
-     - Creates PumpSuspend and PumpResume records to complete missing pairs
-     - Removes any records whose timestamps don't fall into the specified window
-     - Adjusts TempBasalDuration records for overlapping entries
+Tasks performed by this pass:
+ - De-duplicates BolusWizard records
+ - Creates PumpSuspend and PumpResume records to complete missing pairs
+ - Removes any records whose timestamps don't fall into the specified window
+ - Adjusts TempBasalDuration records for overlapping entries
     """
     def get_params(self, args):
         return dict(infile=args.infile, start_datetime=args.start, end_datetime=args.end)
 
     def configure_app(self, app, parser):
-        parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-        parser.add_argument('--start', type=dateparser.parse, default=None)
-        parser.add_argument('--end', type=dateparser.parse, default=None)
+        parser.add_argument(
+            'infile',
+            nargs='?',
+            type=argparse.FileType('r'),
+            default=sys.stdin,
+            help='JSON-encoded history data to clean'
+        )
+        parser.add_argument(
+            '--start',
+            type=dateparser.parse,
+            default=None,
+            help='The initial timestamp of the window to return'
+        )
+        parser.add_argument(
+            '--end',
+            type=dateparser.parse,
+            default=None,
+            help='The final timestamp of the window to return'
+        )
 
     def main(self, args, app):
         params = self.get_params(args)
